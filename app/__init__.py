223d37d80783c675f52a2193e6083d7f182d0290
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-05-22 16:53:56 -0400

    Update the Post-Info Template

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.routes import home
 from app.routes import home, dashboard
 from app.db import init_db
+from app.utils import filters
 
 def create_app(test_config=None):
     # set up app config
@@ -11,6 +12,11 @@ def create_app(test_config=None):
         SECRET_KEY='super_secret_key'
     )
 
+    # Register Jinja filters
+    app.jinja_env.filters['format_url'] = filters.format_url
+    app.jinja_env.filters['format_date'] = filters.format_date
+    app.jinja_env.filters['format_plural'] = filters.format_plural
+    
     @app.route('/hello')
     def hello():
         return 'hello world'
